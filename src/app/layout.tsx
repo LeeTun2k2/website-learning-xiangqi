b6a5b8aa933249bc9f37e9f7269f751fe3950d09
Lê Quang Tùng <lequangtung2002@gmail.com>
2023-08-18 11:15:52 +0700

    Improve SEO: page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Học Cờ Tướng Online: Lớp Học Cờ Tướng Miễn Phí và Chất Lượng',
-  description: 'Tìm hiểu và học cờ tướng trực tuyến miễn phí tại trang web chuyên cung cấp các khóa học cờ tướng dành cho người mới bắt đầu và người chơi nâng cao. Cải thiện chiến thuật, khả năng đánh cờ và hiểu rõ chi tiết về luật chơi cờ tướng cùng những hướng dẫn từ các chuyên gia hàng đầu.',
+  title: 'Cờ Tướng Online: Học Cờ Tướng Miễn Phí và Chất Lượng',
+  description: 'Chuyên cung cấp khóa học cờ tướng online, cải thiện chiến thuật, khả năng đánh cờ và hiểu rõ chi tiết về luật chơi từ những hướng dẫn của các chuyên gia hàng đầu.',
 }
 
 export default function RootLayout({
